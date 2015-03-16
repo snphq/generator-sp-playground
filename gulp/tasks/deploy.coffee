4a785fadc6b588f2c40de03518788724e73b5dfd
Maksim Koretskiy <maksim.koretskiy@saltpepper.ru>
2015-03-16 23:06:37 +0300

    Add snp-gulp-tasks package

--- gulp/tasks/deploy.coffee
+++
@@ -1,7 +0,0 @@
-gulp   = require 'gulp'
-deploy = require 'gulp-gh-pages'
-
-module.exports = ->
-  gulp.src('./dist/**/*')
-  .pipe deploy()
-
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 // Note the new way of requesting CoffeeScript since 1.7.x
 require('coffee-script/register');
+
 // This bootstraps your Gulp's main file
-require('./gulp/gulpfile.coffee');
+gulp = require("gulp");
+tasks = require('snp-gulp-tasks')(gulp);
